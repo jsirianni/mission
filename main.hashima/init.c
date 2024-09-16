2c7b216d1ccf7eb3c67d0ca27db87310854bcfa3
CreepyCrappyShow <creepycrappyshow@gmail.com>
2024-09-16 17:17:02 -0300

    WIP

--- main.hashima/init.c
+++ main.hashima/init.c
@@ -41,13 +41,14 @@ class CustomMission : MissionServer
 		super.OnInit();
 
 		// LXD Weather System
+
 		if ( m_EventManager )
 		{
 			// Min time between events, max time between events, max number of events at the same time (LEAVE IT AT 1).
+
 		 	m_EventManager.Run( 120, 180, 1 );
 		 	// m_EventManager.Run( 400, 650, 1 );
 
-			// Registering events and their probability
 		 	m_EventManager.RegisterEvent( LXDTropicalStorm, 0.15 );
 		 	m_EventManager.RegisterEvent( LXDRainShower, 0.17 );
 		 	m_EventManager.RegisterEvent( LXDCloudySkies, 0.25 );
