edf0af6dfdea3fa0380b068a4de050782d5d7927
Joe Sirianni <joe.sirianni@observiq.com>
2025-02-18 16:55:16 -0500

    Add CI/CD workflows

---
+++ .github/workflows/scripts/release.sh
@@ -0,0 +1,18 @@
+#!/usr/local/env bash
+
+set -e
+
+# Ensure $1 is set
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+release=${1}
+
+echo "Creating release $release"
+
+echo "Uploading main.hashima/areaflags.map to the release"
+gh release upload $release main.hashima/areaflags.map
+
+echo "Release finished"
